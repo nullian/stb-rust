cd14e23cfcc55800e8eaf94c12c306cbf9999d51
yuanhao <yuanhao@minivision.cn>
2023-08-31 15:07:09 +0800

    remove unused code in example

--- stb/examples/main.rs
+++ stb/examples/main.rs
@@ -1,8 +1,6 @@
 use stb_rust::{common::Image, image, image_resize, image_write};
 use std::path::PathBuf;
 fn main() {
-    load_and_write_image();
-
     resize_images();
 }
 
@@ -20,24 +18,6 @@ fn get_image(components: usize) -> (PathBuf, Image<u8>) {
     );
 
     (image_path, image_u8)
-}
-
-fn load_and_write_image() {
-    let (image_path, image_u8) = get_image(3);
-
-    let out_path_dir = PathBuf::from(image_path.parent().unwrap());
-    let out_path_jpg = out_path_dir.join("load_and_write.jpg");
-
-    image_write::write_jpg(&out_path_jpg, &image_u8, 100_i32).expect("write jpg failed");
-
-    let out_path_png = out_path_dir.join("load_and_write.png");
-    image_write::write_png(&out_path_png, &image_u8).expect("write png failed");
-
-    let out_path_bmp = out_path_dir.join("load_and_write.bmp");
-    image_write::write_bmp(&out_path_bmp, &image_u8).expect("write bmp failed");
-
-    let out_path_tga = out_path_dir.join("load_and_write.tga");
-    image_write::write_tga(&out_path_tga, &image_u8).expect("tga jpg failed");
 }
 
 fn resize_images() {
@@ -68,10 +48,8 @@ fn resize_images() {
         assert_eq!(resize.info.components, c);
         assert_eq!(resize.buffer.len(), downsample_w * downsample_h * c);
 
-        let out_path_dir = PathBuf::from(&out_path_dir);
         let out_filename = format!("resize_downsample_{}.jpg\0", c.to_string());
-
-        let out_path_jpg = PathBuf::from(&out_path_dir).join(out_filename.as_str());
+        let out_path_jpg = out_path_dir.join(out_filename.as_str());
         assert!(image_write::write_jpg(&out_path_jpg, &resize, 100).is_ok());
 
         let up_sample_resize = image_resize::resize_u8(&image_u8, upsample_w, upsample_h);
@@ -84,7 +62,7 @@ fn resize_images() {
         assert_eq!(resize.buffer.len(), upsample_w * upsample_h * c);
 
         let out_filename = format!("resize_upsample_{}.jpg\0", c.to_string());
-        let out_path_jpg = PathBuf::from(&out_path_dir).join(out_filename.as_str());
+        let out_path_jpg = out_path_dir.join(out_filename.as_str());
         assert!(image_write::write_jpg(&out_path_jpg, &resize, 100).is_ok());
     }
 }
